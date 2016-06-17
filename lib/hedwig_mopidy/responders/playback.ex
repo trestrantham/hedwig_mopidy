f0e0406bf355ab7b0e4ae4d73c0d3e72b808cd50
Tres Trantham <tres@trestrantham.com>
2016-06-17 01:11:09 -0700

    Test play artist

--- lib/hedwig_mopidy/responders/playback.ex
+++ lib/hedwig_mopidy/responders/playback.ex
@@ -1,7 +1,20 @@
 defmodule HedwigMopidy.Responders.Playback do
   use Hedwig.Responder
 
-  hear ~r/play artist/i, msg do
-    send msg, "play artist"
+  hear ~r/play artist (?<artist>.*)/i, message do
+    artist = message.matches["artist"]
+
+    response = 
+      with {:ok, %Mopidy.SearchResult{} = search_results} <- Mopidy.Library.search(%{artist: [artist]}),
+           {:ok, :success} <- Mopidy.Tracklist.clear,
+           {:ok, tracks} when is_list(tracks) <- Mopidy.Tracklist.add(search_results.tracks |> Enum.map(fn(%Mopidy.Track{} = track) -> track.uri end)),
+           {:ok, :success} <- Mopidy.Playback.play do
+        "Playing music by " <> String.capitalize(artist)
+      else
+        {:error, error_message} -> error_message
+        _                       -> "Couldn't play music by that artist"
+      end
+
+    send message, response
   end
 end
--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule HedwigMopidy.Mixfile do
   defp deps do
     [
       {:hedwig, "~> 1.0.0-rc.4"},
-      {:mopidy, git: "git@github.com:trestrantham/mopidy.git"}
+      {:mopidy, git: "git@github.com:trestrantham/mopidy.git", branch: "development"}
     ]
   end
 end
