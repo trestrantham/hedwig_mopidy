3dc615be8c7bec89449588485855cb6515bdbc54
Tres Trantham <tres@trestrantham.com>
2016-06-17 20:41:12 -0700

    Don't use separate supervisor

--- lib/hedwig_mopidy.ex
+++ lib/hedwig_mopidy.ex
@@ -2,6 +2,9 @@ defmodule HedwigMopidy do
   use Application
 
   def start(_type, _args) do
-    HedwigMopidy.Supervisor.start_link
+    import Supervisor.Spec, warn: false
+
+    opts = [strategy: :one_for_one, name: HedwigMopidy.Supervisor]
+    Supervisor.start_link([], opts)
   end
 end
