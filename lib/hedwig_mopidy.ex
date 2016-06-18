0267ab0be2c46de12d118049edd81c4fec65106c
Tres Trantham <tres@trestrantham.com>
2016-06-17 22:10:22 -0700

    Add additional listeners

--- lib/hedwig_mopidy.ex
+++ lib/hedwig_mopidy.ex
@@ -7,4 +7,18 @@ defmodule HedwigMopidy do
     opts = [strategy: :one_for_one, name: HedwigMopidy.Supervisor]
     Supervisor.start_link([], opts)
   end
+
+  def playing_string(%Mopidy.Track{} = track) do
+    "♫ Playing " <> HedwigMopidy.track_string(track)
+  end
+
+  def track_string(%Mopidy.Track{} = track) do
+    track.name <> " by " <> artists_string(track.artists)
+  end
+
+  def artists_string(List = artists) do
+    artists
+    |> Enum.map(fn artist -> artist.name end)
+    |> Enum.join(", ")
+  end
 end
--- lib/hedwig_mopidy/responders/playback.ex
+++ lib/hedwig_mopidy/responders/playback.ex
@@ -9,12 +9,101 @@ defmodule HedwigMopidy.Responders.Playback do
            {:ok, :success} <- Mopidy.Tracklist.clear,
            {:ok, tracks} when is_list(tracks) <- Mopidy.Tracklist.add(search_results.tracks |> Enum.map(fn(%Mopidy.Track{} = track) -> track.uri end)),
            {:ok, :success} <- Mopidy.Playback.play do
-        "Playing music by " <> String.capitalize(artist)
+        "♫ Playing music by " <> String.capitalize(artist)
       else
         {:error, error_message} -> error_message
-        _                       -> "Couldn't play music by that artist"
+        _ ->
+          case Mopidy.Tracklist.get_length do
+            {:ok, 0} -> "✗ Couldn't find any music for that artist"
+            _        -> "✗ Couldn't play music by that artist"
+          end
       end
 
     send message, response
   end
+
+  hear ~r/play album (?<album>.*) by (?<artist>.*)/i, message do
+    album = message.matches["album"]
+    artist = message.matches["artist"]
+
+    response = 
+      with {:ok, %Mopidy.SearchResult{} = search_results} <- Mopidy.Library.search(%{artist: [artist], album: [album]}),
+           {:ok, :success} <- Mopidy.Tracklist.clear,
+           {:ok, tracks} when is_list(tracks) <- Mopidy.Tracklist.add(search_results.tracks |> Enum.map(fn(%Mopidy.Track{} = track) -> track.uri end)),
+           {:ok, :success} <- Mopidy.Playback.play do
+        "♫ Playing " <> String.capitalize(album) <> " by " <> String.capitalize(artist)
+      else
+        {:error, error_message} -> error_message
+        _ ->
+          case Mopidy.Tracklist.get_length do
+            {:ok, 0} -> "✗ Couldn't find any music for that album"
+            _        -> "✗ Couldn't play music for that album"
+          end
+      end
+
+    send message, response
+  end
+
+  hear ~r/(who is|who's) playing/i, message do
+    response = 
+      with {:ok, current_track} <- Mopidy.Playback.get_current_track do
+        case current_track do
+          %Mopidy.Track{} = track -> HedwigMopidy.playing_string(track)
+          _ -> "■ Nothing is playing"
+        end
+      else
+        {:error, error_message} -> error_message
+        _ -> "✗ Couldn't find who's playing"
+      end
+
+    send message, response
+  end
+
+  hear ~r/play/i, message do
+    response = 
+      with {:ok, :success} <- Mopidy.Playback.play,
+           {:ok, current_track} <- Mopidy.Playback.get_current_track do
+        case current_track do
+          %Mopidy.Track{} = track -> HedwigMopidy.playing_string(track)
+          _ -> "► No songs to play"
+        end
+      else
+        {:error, error_message} -> error_message
+        _ -> "✗ Couldn't play music"
+      end
+
+    send message, response
+  end
+
+  hear ~r/stop/i, message do
+    response = 
+      with {:ok, :success} <- Mopidy.Playback.stop do
+        "■ Stopped"
+      else
+        {:error, error_message} -> error_message
+        _ -> "✗ Couldn't stop music"
+      end
+
+    send message, response
+  end
+
+  hear ~r/pause/i, message do
+    response = 
+      with {:ok, :success} <- Mopidy.Playback.pause,
+           {:ok, state} <- Mopidy.Playback.get_state,
+           {:ok, current_track} <- Mopidy.Playback.get_current_track do
+        case state do
+          "playing" -> HedwigMopidy.playing_string(current_track)
+          "stopped" -> "Stopped"
+          "paused" -> "Paused on " <> HedwigMopidy.track_string(current_track)
+          _ -> "✗ Couldn't pause music"
+        end
+      else
+        {:error, error_message} -> error_message
+        _ -> "✗ Couldn't pause music"
+      end
+
+    send message, response
+  end
+
 end
